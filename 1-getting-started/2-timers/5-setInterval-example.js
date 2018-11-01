3cedfb936189119671e7ed7fe54939a35dfbce83
hari <hari.k.krishna4@gmail.com>
2018-11-01 14:25:29 -0700

    second timeout challenge

--- 1-getting-started/2-timers/5-setInterval-example.js
+++ 1-getting-started/2-timers/5-setInterval-example.js
@@ -2,3 +2,4 @@ setInterval(
   () => console.log('Hello every 3 seconds'),
   3000
 );
+setImmediate(setInterval);
--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,14 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+let counter = 0;
+const interval = setInterval(
+    () => {
+        console.log("Hello World");
+        counter += 1;
+        if (counter === 5) {
+            console.log('Done');
+            clearInterval(interval);
+        }
+    }, 1000
+);
